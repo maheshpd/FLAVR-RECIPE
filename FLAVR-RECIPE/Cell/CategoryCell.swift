2d79d882b710d08e18e9d565ac9917d2a45fd1c0
Mahesh Prasad <mahesh223prasad@gmail.com>
2019-07-08 23:43:03 +0530

    Category section Done

---
+++ FLAVR-RECIPE/Cell/CategoryCell.swift
@@ -0,0 +1,25 @@
+//
+//  CategoryCell.swift
+//  FLAVR-RECIPE
+//
+//  Created by Mahesh Prasad on 08/07/19.
+//  Copyright © 2019 CreatesApps. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryLbl: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
